2f55b35d88ca546695f6e469a0d7eeb33efcb853
anushas2504 <anushas2504@gmail.com>
2026-06-10 16:34:52 +0530

    Fix import paths for Home and Admin components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-import Home from "@/pages/Home";
-import Admin from "@/pages/Admin";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 export default function App() {
   return (
